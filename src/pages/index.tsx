99b42ae29fa90c647156d2724ee5263b07cc4845
Bruno Luis <bruno-aqv@outlook.com>
2021-03-01 18:46:00 -0300

    Profile completo :heavy_check_mark:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Profile } from "../components/Profile";
 import { ExperienceBar } from "../components/ExperienceBar";
 import styles from "../styles/pages/Home.module.css";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,6 +10,7 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
         </div>
         <div></div>
       </section>
